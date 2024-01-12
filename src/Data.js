22b75687a80c9108170497f3c758302e7b639589
F3lOh <126726529+DevOpF3loH@users.noreply.github.com>
2024-01-12 14:40:38 +0300

    updates

--- src/Data.js
+++ src/Data.js
@@ -145,7 +145,7 @@ export const userRows = [
 export const productRows = [
   {
     id: 1,
-    name: "Apple Airpods",
+    name: "Apple Airpods Test",
     img: "https://images.unsplash.com/photo-1592921870583-aeafb0639ffe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
     stock: 123,
     status: "available",
